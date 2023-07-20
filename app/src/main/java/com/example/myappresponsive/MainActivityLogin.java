3f5dd63547bca1f17dbe5beb487d6535455b17a9
dquinteroa1 <dquinteroa@uteq.edu.ec>
2023-07-20 14:33:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myappresponsive/MainActivityLogin.java
@@ -0,0 +1,14 @@
+package com.example.myappresponsive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
