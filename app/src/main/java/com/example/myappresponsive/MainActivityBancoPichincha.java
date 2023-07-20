e2026037039f72aec5e871375dc4e3c354249c11
dquinteroa1 <dquinteroa@uteq.edu.ec>
2023-07-20 16:43:05 -0500

    Login Bancho Pichincha

---
+++ app/src/main/java/com/example/myappresponsive/MainActivityBancoPichincha.java
@@ -0,0 +1,14 @@
+package com.example.myappresponsive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityBancoPichincha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_banco_pichincha);
+    }
+}
